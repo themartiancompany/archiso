fce802c5ce7ad1e798d62f1237f4dcca1e2f4c49
Pellegrino Prevete <pellegrinoprevete@gmail.com>
2022-06-08 07:14:01 +0000

    updated profiledef.sh

--- configs/releng/profiledef.sh
+++ configs/releng/profiledef.sh
@@ -8,17 +8,26 @@ iso_application="Arch Linux Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-	   'uefi-ia32.grub.esp' 'uefi-x64.systemd-boot.esp'
-	   'uefi-ia32.grub.eltorito' 'uefi-x64.systemd-boot.eltorito')
+bootmodes=('bios.syslinux.mbr'
+           'bios.syslinux.eltorito'
+	   'uefi-ia32.grub.esp'
+           'uefi-ia32.grub.eltorito'
+           'uefi-x64.systemd-boot.esp'
+           'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86'
+                             '-b' '1M' '-Xdict-size' '1M')
+encryption_key=""
+keys_image_type="squashfs+luks"
+keys_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86'
+			 '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
+  ["/run/archiso/keys"]="0:0:700"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
