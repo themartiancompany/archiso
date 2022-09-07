dfa280b7b8565a66ebedab6e62ccb4408c25bccc
Pellegrino Prevete <pellegrinoprevete@gmail.com>
2022-09-07 09:53:50 +0200

    remove syslinux from profiles

--- configs/releng/profiledef.sh
+++ configs/releng/profiledef.sh
@@ -8,12 +8,12 @@ iso_application="Arch Linux Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso' 'dongle')
-bootmodes=('bios.syslinux.mbr'
-           'bios.syslinux.eltorito'
+bootmodes=('bios.grub.mbr'
+           'bios.grub.eltorito'
 	   'uefi-ia32.grub.esp'
            'uefi-ia32.grub.eltorito'
-           'uefi-x64.systemd-boot.esp'
-           'uefi-x64.systemd-boot.eltorito')
+           'uefi-x64.grub.esp'
+           'uefi-x64.grub.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="erofs"
